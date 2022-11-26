00cea509e02f64a14c36a22f609596af3c56e3ec
anup kumar <kanup414@gmail.com>
2022-11-26 18:14:22 +0530

    Explain with code

--- block-BNaaat/server.js
+++ block-BNaaat/server.js
@@ -6,13 +6,22 @@ var server = http.createServer(handleRequest);
 
 function handleRequest (req, res) {
     if(req.method === 'GET' && req.url === '/file') {
-        res.end('Welcome to index page')
-    }
-    if(req.method === 'GET' && req.url === '/stream') {
-        res.setHeader('Content-Type', 'text/html');
-        fs.createReadStream('./about.html').pipe(res);
+        fs.readFile('./node.html', (err, content) => {
+            if(err) console.log(err);
+            res.setHeader('Content-Type', 'text/html');
+            res.end(content);
+        })
 
+        if(req.method === 'GET' && req.url === '/stream') {
+            res.setHeader('Content-Type', 'text/html');
+            fs.createReadStream('./node.html').pipe(res);
+        }
     }
+    // if(req.method === 'GET' && req.url === '/stream') {
+    //     res.setHeader('Content-Type', 'text/html');
+    //     fs.createReadStream('./about.html').pipe(res);
+
+    // }
 }
 
 server.listen(5555, () => {
