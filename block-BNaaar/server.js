43fef0dac6b034fe8c30dce6188be182dfb39de2
anup kumar <kanup414@gmail.com>
2022-11-26 17:27:33 +0530

    Explain with code

--- block-BNaaar/server.js
+++ block-BNaaar/server.js
@@ -8,13 +8,14 @@ var server = http.createServer(handleRequest);
 function handleRequest (req, res) {
     console.log(req.method, req.url);
     if(req.method === 'GET' && req.url === '/') {
+        res.setHeader('Content-Type', 'text/plain')
         res.write('Welocme to homepage');
         res.end();
     } else if(req.method === 'GET' && req.url === '/about') {
         res.setHeader('Content-Type', 'text/html')
         res.end('<h2>this is all about NodeJS</h2>')
     } else if(req.method === 'POST' && req.url === '/about') {
-        res.writeHead(404, {'Content-Type': 'text/html'})
+        res.writeHead(404, {'Content-Type': 'application/json'})
         res.write('{message: this is a post request}');
         res.end();
     }
